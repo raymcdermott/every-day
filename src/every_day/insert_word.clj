98c47eb469d8d17f9836a68d8c0f7d0ee435bcfb
Ray McDermott <ray@mcdermott.be>
2018-01-30 00:58:42 +0100

    actually works

--- src/every_day/insert_word.clj
+++ src/every_day/insert_word.clj
@@ -33,23 +33,44 @@
         (vec kws-rand)
         (recur (conj kws-rand (rand-nth keywords)))))))
 
-(defn get-replacements
-  [replacements quotation]
-  (let [[part-1 part-2] (n-parts (set (syntax quotation)) (set replacers/replacement-parts) 2)
-        [degree-1 degree-2] (n-degrees replacers/badness-degrees 2)]
-    [(assoc {} part-1 (get (get replacements degree-1) part-1))
-     (assoc {} part-2 (get (get replacements degree-2) part-2))]))
+(defn syntax-match-rand-nth
+  "Give rand-nth on index/es where a given syntax part is located in a given syntax"
+  [syntax part]
+  (rand-nth (:positions (reduce
+                          (fn [w1 w2]
+                            (let [{:keys [index match positions]} w1]
+                              (if (= match w2)
+                                (assoc w1 :index (inc index) :positions (conj positions index))
+                                (assoc w1 :index (inc index)))))
+                          {:index 0 :match part :positions []} syntax))))
+
+(defn quote-replace-in
+  "Inject the replacements into the quote at the given spots"
+  [q replacements]
+  (:output (reduce
+             (fn [w1 w2]
+               (let [{:keys [index replacements output]} w1
+                     replace-1 (first replacements)
+                     replace-2 (last replacements)]
+                 (cond
+                   (= (:pos replace-1) index) (assoc w1 :index (inc index) :output (conj output (:replacement replace-1)))
+                   (= (:pos replace-2) index) (assoc w1 :index (inc index) :output (conj output (:replacement replace-2)))
+                   :else (assoc w1 :index (inc index) :output (conj output w2)))))
+             {:index 0 :replacements replacements :output []} q)))
 
 (defn syntax-replace
   [replacements quotation]
-  (let [[replace-1 replace-2] (get-replacements replacements quotation)]
-    ; REMOVE DUPS! - maybe reduce rather than map
-    (map (fn [original first-opt second-opt]
-           (cond
-             (= (keys original) (keys first-opt)) first-opt
-             (= (keys original) (keys second-opt)) second-opt
-             :else original))
-         quotation (repeat replace-1) (repeat replace-2))))
+  (let [quote-syntax (syntax quotation)
+        [part-1 part-2] (n-parts (set quote-syntax)
+                                 (set replacers/replacement-parts) 2)
+        [degree-1 degree-2] (n-degrees replacers/badness-degrees 2)
+        replace-1    (assoc {} part-1 (get (get replacements degree-1) part-1))
+        replace-2    (assoc {} part-2 (get (get replacements degree-2) part-2))
+        pos-1        (syntax-match-rand-nth quote-syntax part-1)
+        pos-2        (syntax-match-rand-nth quote-syntax part-2)]
+
+    (quote-replace-in quotation [{:pos pos-1 :replacement replace-1}
+                                 {:pos pos-2 :replacement replace-2}])))
 
 (defn replace-all
   [replacements quotes]
@@ -68,4 +89,4 @@
   (let [qs (random-repeat-replacements replacements quotes repetitions)]
     (map (comp clojure.string/join (partial clojure.string/join " ")) qs)))
 
-(display-replacements replacers/potus quotes/qs 10)
+(display-replacements replacers/potus quotes/qs 1)
